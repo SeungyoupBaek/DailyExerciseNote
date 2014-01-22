6b486e035f56f53feaf4d2645d4cc73671975aa1
SeungyoupBaek <bsy8220@naver.com>
2014-01-22 13:32:08 +0900

    피커를 띄웠다.

--- DailyExerciseNote/CountingViewController.m
+++ DailyExerciseNote/CountingViewController.m
@@ -8,11 +8,60 @@
 
 #import "CountingViewController.h"
 
-@interface CountingViewController ()
+
+@interface CountingViewController () <UIPickerViewDataSource, UIPickerViewDelegate>{
+    NSArray *_exerciseList;
+    UIPickerView *_picker;
+    UIActionSheet *_sheet;
+    float _height;
+}
 
 @end
 
 @implementation CountingViewController
+
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 1;
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    return _exerciseList.count;
+}
+
+- (void)handleDone:(id)sender{
+    [_sheet dismissWithClickedButtonIndex:0 animated:YES];
+    
+}
+
+- (IBAction)showExerciseList:(id)sender {
+    CGSize viewSize = self.view.bounds.size;
+    
+    if (nil == _sheet) {
+        _sheet = [[UIActionSheet alloc] init];
+        
+        // 툴바와 Done 버튼
+        UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, viewSize.width, 44)];
+        UIBarButtonItem *done = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(handleDone:)];
+        
+        NSArray *items = [NSArray arrayWithObject:done];
+        [toolbar setItems:items];
+        _picker = [[UIPickerView alloc] initWithFrame:CGRectMake(0, toolbar.frame.size.height, viewSize.width, _picker.frame.size.height)];
+        
+        // 시트에 추가
+        [_sheet addSubview:toolbar];
+        [_sheet addSubview:_picker];
+        
+        // 시트 나타나기
+        [_sheet showInView:self.view];
+        
+        // 액션시트크기와 위치계산용
+        _height = toolbar.frame.size.height + _picker.frame.size.height;
+    }
+    
+    [_sheet showInView:self.view];
+    _sheet.frame = CGRectMake(0, viewSize.height - _height, viewSize.width, _height);
+    
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -26,6 +75,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    _exerciseList = @[@"Squat", @"BenchPress", @"PullUp", @"DeadLift", @"LegLaise"];
+    _picker = [[UIPickerView alloc] init];
 	// Do any additional setup after loading the view.
 }
 
