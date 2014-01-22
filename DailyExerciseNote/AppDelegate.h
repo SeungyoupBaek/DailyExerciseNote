0bf81e09102470fe203530fe3bfa699c65dbaf62
SeungyoupBaek <bsy8220@naver.com>
2014-01-22 11:27:28 +0900

    Calender OpenSource 가져왔다.

--- DailyExerciseNote/AppDelegate.h
+++ DailyExerciseNote/AppDelegate.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CKCalendarView.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
--- DailyExerciseNote/ViewController.m
+++ DailyExerciseNote/ViewController.m
@@ -5,18 +5,40 @@
 //  Created by Youp on 2014. 1. 22..
 //  Copyright (c) 2014년 admin. All rights reserved.
 //
-
+#import <CoreGraphics/CoreGraphics.h>
 #import "ViewController.h"
+#import "CKCalendarView.h"
 
-@interface ViewController ()
+@interface ViewController ()<CKCalendarDelegate>
+
 
 @end
 
 @implementation ViewController
 
+
+- (BOOL)calendar:(CKCalendarView *)calendar willSelectDate:(NSDate *)date {
+    // don't let people select dates in previous/next month
+    return [calendar dateIsInCurrentMonth:date];
+}
+
+
+- (void)calendar:(CKCalendarView *)calendar didSelectDate:(NSDate *)date {
+    
+}
+
+
+- (void)calendar:(CKCalendarView *)calendar didDeselectDate:(NSDate *)date {
+    NSLog(@"User didn't like date %@", date);
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    CKCalendarView *calendar = [[CKCalendarView alloc] init];
+    [self.view addSubview:calendar];
+    calendar.delegate = self;
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
